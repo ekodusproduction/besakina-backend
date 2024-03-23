3c657c6c3d0812f0e7c2095b2e54fdc94a192ac1
jay-ekodus <jsardar@ekodusinc.com>
2024-03-23 12:12:54 +0530

    added env

--- src/Mysql/mysql.database.js
+++ src/Mysql/mysql.database.js
@@ -3,11 +3,11 @@ import dotenv from 'dotenv';
 dotenv.config();
 console.log(process.env)
 const pool = mysql.createPool({
-    host: process.env.MDB_HOST,
-    port: process.env.MDB_PORT,
-    user: process.env.MDB_USER,
-    password: process.env.MDB_PASS,
-    database: process.env.MDB_DATABASE || "besakina",
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE || "besakina",
     connectionLimit: 100,
 }).promise();
 
