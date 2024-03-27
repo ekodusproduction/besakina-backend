fb7e199fd292d96318178045ef1aed4f4f7730ee
jay-ekodus <jsardar@ekodusinc.com>
2024-03-27 10:58:57 +0530

    added console-4

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 })
 app.use('/public', express.static('public'));
 // Route definitions
-app.get("/", (req, res) => {
+app.get("/api", (req, res) => {
     res.status(200).send("Welcome to besakina backend server");
 });
 
