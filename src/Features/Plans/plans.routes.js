a11230cc60640c0f87af8a5070ee58847196fbf8
jay-ekodus <jsardar@ekodusinc.com>
2024-03-28 17:58:28 +0530

    fix migration-4

--- src/Features/Plans/plans.routes.js
+++ src/Features/Plans/plans.routes.js
@@ -2,9 +2,10 @@ import { Router } from "express";
 import { addPlan, deletePlan, getPlan } from "./plans.controller.js";
 import { fileUpload } from "../../Middlewares/multer.middlewares.js";
 import { addPlanValidator } from "./plans.validator.js";
+import { jwtAuth } from "../../Middlewares/auth.middleware.js";
 const plansRouter = Router()
 
-plansRouter.post("/", fileUpload('images'), addPlanValidator, addPlan)
+plansRouter.post("/",jwtAuth, fileUpload('images'), addPlanValidator, addPlan)
 plansRouter.get("/", getPlan)
 // plansRouter.put("/id/:id", deletePlan)
 plansRouter.delete("/id/:id", deletePlan)
