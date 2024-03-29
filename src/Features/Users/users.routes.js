870fd68bf0ed2ad4cedb5114bcdc2b21d9362e63
jay-ekodus <jsardar@ekodusinc.com>
2024-03-29 14:15:49 +0530

    added plans-5

--- src/Features/Users/users.routes.js
+++ src/Features/Users/users.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { loginValidation, mobileValidation, getUsers } from "./users.validator.js";
-import { login, sendOtp } from "./users.controller.js";
+import { loginValidation, mobileValidation } from "./users.validator.js";
+import { login, sendOtp ,getUsers} from "./users.controller.js";
 import { jwtAuth } from "../../Middlewares/auth.middleware.js";
 const userRouter = Router()
 
