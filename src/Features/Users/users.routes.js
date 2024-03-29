ae0c1d158d60745ce9e7bfe82d63de2c30742b52
jay-ekodus <jsardar@ekodusinc.com>
2024-03-29 13:22:14 +0530

    added plans-4

--- src/Features/Users/users.routes.js
+++ src/Features/Users/users.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { loginValidation, mobileValidation } from "./users.validator.js";
 import { login, sendOtp } from "./users.controller.js";
+import { jwtAuth } from "../../Middlewares/auth.middleware.js";
 const userRouter = Router()
 
 userRouter.post("/login", loginValidation, login)
 userRouter.post("/sendotp", mobileValidation, sendOtp)
+userRouter.get("/", jwtAuth, getUsers)
 
 export default userRouter  
