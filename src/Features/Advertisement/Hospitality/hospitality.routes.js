18c8406602b217bac325c0f9acb7420cda63480a
jay-ekodus <jsardar@ekodusinc.com>
2024-04-01 15:08:06 +0530

    added plans -2

--- src/Features/Advertisement/Hospitality/hospitality.routes.js
+++ src/Features/Advertisement/Hospitality/hospitality.routes.js
@@ -11,7 +11,7 @@ import { checkPlanValidity } from "../../../Middlewares/checkValidPlan.middlewar
 const hospitalityRouter = Router()
 
 //protected routes id=> advertisement id
-hospitalityRouter.post("/", jwtAuth,checkPlanValidity, fileUpload("images"), validationMiddlewarePost, addAdvertisement)
+hospitalityRouter.post("/", jwtAuth, fileUpload("images"), validationMiddlewarePost, addAdvertisement)
 
 hospitalityRouter.get("/filter", filterAdvertisement)
 
