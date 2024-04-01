b625dfe2060f311e55386b6f6934b04200826697
jay-ekodus <jsardar@ekodusinc.com>
2024-04-01 15:12:47 +0530

    added plans -4

--- src/Features/Advertisement/Hospitality/hospitality.table.js
+++ src/Features/Advertisement/Hospitality/hospitality.table.js
@@ -28,6 +28,8 @@ export const createHospitalityTable = async function () {
             latitude DECIMAL(10, 8) ,
             longitude DECIMAL(11, 8) ,
 
+            is_active BOOLEAN DEFAULT 1,
+
             created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
             updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
             
