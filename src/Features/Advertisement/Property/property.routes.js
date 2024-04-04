0d8b0103519344cd74b7d4248a13e5841e5679f9
jay-ekodus <jsardar@ekodusinc.com>
2024-04-04 14:53:25 +0530

    added user selection with get i

--- src/Features/Advertisement/Property/property.routes.js
+++ src/Features/Advertisement/Property/property.routes.js
@@ -16,21 +16,24 @@ propertyRouter.post("/add", jwtAuth, fileUpload("property"), addAdvertisement)
 
 propertyRouter.get("/filter", filterAdvertisement)
 
+propertyRouter.get("/id/:id", getAdvertisement)
 propertyRouter.put("/id/:id", jwtAuth, validationMiddlewarePut, updateAdvertisement)
-propertyRouter.put("/activate/id/:id", jwtAuth, activateAdvertisement)
 
+propertyRouter.put("/activate/id/:id", jwtAuth, activateAdvertisement)
 propertyRouter.delete("/deactivate/id/:id", jwtAuth, deleteAdvertisement)
 
-propertyRouter.get("/id/:id", getAdvertisement)
+
 // images
 //id =>advertisement id
+propertyRouter.post("/images/:id", jwtAuth, fileUpload("images"), imageValidator, addImage)
+
+
 propertyRouter.delete("/image/delete/:id", jwtAuth, deleteImage)
 
-propertyRouter.post("/images/:id", jwtAuth, fileUpload("images"), imageValidator, addImage)
 // list user own advertisement //id => user id
+propertyRouter.get("/list", getListAdvertisement)
 propertyRouter.get("/list/self", jwtAuth, listUserAdvertisement)
 //category => doctors, education, hospitals, hospitality, vehicles, properties
-propertyRouter.get("/list", getListAdvertisement)
 
 
 
