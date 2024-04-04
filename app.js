2d77cb177e0f86f23de75b0c93a624b8e43f3188
jay-ekodus <jsardar@ekodusinc.com>
2024-04-04 11:34:23 +0530

    added doctors-2

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.use('/api/public', (req, res) => {
+app.use('/api/public', (req, res, next) => {
     // Check if the request origin matches the allowed origin
     const allowedOrigin = req.headers.origin === 'http://localhost:5173' ? req.headers.origin : '*';
     // Set the Access-Control-Allow-Origin header
@@ -52,6 +52,7 @@ app.use('/api/public', (req, res) => {
     // Set other CORS headers
     res.setHeader('Access-Control-Allow-Methods', 'GET');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    next()
 });
 
 console.log('request before files')
