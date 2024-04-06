a944789370ca9318377daf88bf74c5cf790d8071
jay-ekodus <jsardar@ekodusinc.com>
2024-04-06 11:45:48 +0530

    added user selection with get -122

--- src/Features/Users/users.routes.js
+++ src/Features/Users/users.routes.js
@@ -2,12 +2,13 @@ import { Router } from "express";
 import { loginValidation, mobileValidation  } from "./users.validator.js";
 import { login, sendOtp, getUsers, userDetails } from "./users.controller.js";
 import { jwtAuth } from "../../Middlewares/auth.middleware.js";
+import { fileUpload } from "../../Middlewares/multer.middlewares.js";
 const userRouter = Router()
 
 userRouter.get("/", jwtAuth, getUsers)
 
 userRouter.post("/login", loginValidation, login)
 userRouter.post("/sendotp", mobileValidation, sendOtp)
-userRouter.post("/details", ,userDetails)
+userRouter.post("/details", fileUpload("users"),userDetails)
 
 export default userRouter  
