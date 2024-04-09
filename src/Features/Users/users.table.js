5719e5aca2242e776dfa1e02b6e716acd90cb895
jay-ekodus <jsardar@ekodusinc.com>
2024-04-09 12:08:08 +0530

    fixed plans-5

--- src/Features/Users/users.table.js
+++ src/Features/Users/users.table.js
@@ -6,38 +6,30 @@ export const createUserTable = async function () {
         const connection = await pool.getConnection();
 
         // Define your CREATE TABLE query
-        const createTableQuery = `
-        CREATE TABLE IF NOT EXISTS users (
-        id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-        plan_id BIGINT UNSIGNED,
-        
-        fullname VARCHAR(255),
-        mobile BIGINT UNSIGNED NOT NULL UNIQUE,
-        otp INT,
-        email VARCHAR(255),
-        aadhar_number VARCHAR(50),
-        pan_number VARCHAR(50),
-
-        doc_number VARCHAR(255),
-        doc_type VARCHAR(255),
-        doc_file VARCHAR(255),
-        
-        profile_pic VARCHAR(255),
-        plan_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        contacts_quota INT, 
-        business_image TEXT,
-        
-        state VARCHAR(255),
-        city VARCHAR(255),
-        locality VARCHAR(255),
-        pincode VARCHAR(255),
-        about VARCHAR(255),
-
-        FOREIGN KEY (plan_id) REFERENCES plans(id) ON DELETE CASCADE,
-        createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-      );
-    `;
+        const createTableQuery = `CREATE TABLE IF NOT EXISTS users (
+            id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+            plan_id BIGINT UNSIGNED,
+            fullname VARCHAR(255),
+            mobile BIGINT UNSIGNED NOT NULL UNIQUE,
+            otp INT,
+            email VARCHAR(255),
+            aadhar_number VARCHAR(50),
+            pan_number VARCHAR(50),
+            doc_number VARCHAR(255),
+            doc_type VARCHAR(255),
+            doc_file VARCHAR(255),
+            profile_pic VARCHAR(255),
+            plan_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            contacts_quota INT, 
+            state VARCHAR(255),
+            city VARCHAR(255),
+            locality VARCHAR(255),
+            pincode VARCHAR(255),
+            about VARCHAR(255),
+            FOREIGN KEY (plan_id) REFERENCES plans(id) ON DELETE CASCADE,
+            createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+        );`;
 
         // Execute the query
         const [results, fields] = await connection.query(createTableQuery);
