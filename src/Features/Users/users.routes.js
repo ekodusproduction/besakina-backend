6680b6b637b842a0a1b89b08b0e8218792b2625f
jay-ekodus <jsardar@ekodusinc.com>
2024-04-09 17:10:28 +0530

    added get all ads -3

--- src/Features/Users/users.routes.js
+++ src/Features/Users/users.routes.js
@@ -1,6 +1,6 @@
 import { Router } from "express";
-import { loginValidation, mobileValidation  } from "./users.validator.js";
-import { login, sendOtp, getUsers, userDetails } from "./users.controller.js";
+import { loginValidation, mobileValidation } from "./users.validator.js";
+import { login, sendOtp, getUsers, userDetails, getUserAdds } from "./users.controller.js";
 import { jwtAuth } from "../../Middlewares/auth.middleware.js";
 import { fileUpload } from "../../Middlewares/multer.middlewares.js";
 const userRouter = Router()
@@ -9,7 +9,7 @@ userRouter.get("/", jwtAuth, getUsers)
 
 userRouter.post("/login", loginValidation, login)
 userRouter.post("/sendotp", mobileValidation, sendOtp)
-userRouter.post("/details", fileUpload("users"),userDetails)
-userRouter.get("/myads",)
+userRouter.post("/details", jwtAuth, fileUpload("users"), userDetails)
+userRouter.get("/myads", jwtAuth, getUserAdds)
 
 export default userRouter  
