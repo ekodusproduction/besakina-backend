4def0c75f8c96663bc3b20d6b5be84dc9fb6137d
jay-ekodus <jsardar@ekodusinc.com>
2024-04-10 13:15:11 +0530

    updated vehicles-10

--- src/Features/Users/users.controller.js
+++ src/Features/Users/users.controller.js
@@ -109,6 +109,9 @@ export const userDetails = async function (req, res, next) {
         
         console.log("body", req.body);
         console.log("files", req.files);
+        console.log("docFile", req.docFile);
+        console.log("files", req.docFile);
+        console.log("profilePic", req.docFile);
 
         // Add profile_pic path to requestBody
         if (profilePic) {
