01c760d1fe65e96463241816e9e9c0f46ae5c213
jay-ekodus <jsardar@ekodusinc.com>
2024-04-10 15:49:46 +0530

    updated vehicles-19

--- src/Features/Users/sql.js
+++ src/Features/Users/sql.js
@@ -1,34 +1,39 @@
-export const getAllPosts = `(SELECT id, 'doctors' AS type, title, description, name AS doctor_name, city, state, images 
-FROM doctors 
-WHERE user_id = ?)
-
-UNION ALL
-(
-SELECT id, 'education' AS type, title, description, institution_name , city, state, images 
-FROM education 
-WHERE user_id = ?
+export const getAllPosts = `(
+    SELECT id, title, price, created_at, images,city, state, 'property' AS category
+    FROM property
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-SELECT id, 'hospitality' AS type, title, description,name AS hospitality_name, city, state, images 
-FROM hospitality 
-WHERE user_id = ?
+    SELECT id, title, price, created_at, images,city, state, 'vehicles' AS category
+    FROM vehicles
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-SELECT id, 'hospitals' AS type, title, description, name AS hospital_name, city, state, images 
-FROM hospitals 
-WHERE user_id = ?
+    SELECT id, title, price, created_at, images,city, state, 'hospitality' AS category
+    FROM hospitality
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-SELECT id, 'property' AS type, title, description, price, city, state, images 
-FROM property 
-WHERE user_id = ?
+    SELECT id, title, price, created_at, images,city, state, 'education' AS category
+    FROM education
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-SELECT id, 'vehicles' AS type, title, description, price, city, state, images 
-FROM vehicles 
-WHERE user_id = ?
-);`
+    SELECT id, title, price_per_visit, created_at, images,city, state, 'doctors' AS category
+    FROM doctors WHERE user_id = ?
+)
+UNION ALL
+(
+    SELECT id, title, price_registration, created_at, images,city, state, 'hospitals' AS category
+    FROM hospitals WHERE user_id = ?
+)
+ORDER BY FIELD(category, 'property', 'vehicles', 'hospitality', 'education', 'doctors', 'hospitals'), created_at DESC
+LIMIT ? OFFSET ?;`
