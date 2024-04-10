32e64a6fb61e54e739ab9e2ae622adf443c3b2af
jay-ekodus <jsardar@ekodusinc.com>
2024-04-10 15:23:33 +0530

    updated vehicles-17

--- src/Features/Users/sql.js
+++ src/Features/Users/sql.js
@@ -1,12 +1,12 @@
 export const getAllPosts = `SELECT id, 'doctors' AS type, title, description,price,city,state, images FROM doctors WHERE user_id = ?
 UNION ALL
-SELECT id, 'education' AS type, title, description, images FROM education WHERE user_id = ?
+SELECT id, 'education' AS type, title, description,price,city,state, images FROM education WHERE user_id = ?
 UNION ALL
-SELECT id, 'hospitality' AS type, title, description, images FROM hospitality WHERE user_id = ?
+SELECT id, 'hospitality' AS type, title, description,price,city,state, images FROM hospitality WHERE user_id = ?
 UNION ALL
-SELECT id, 'hospitals' AS type, title, description, images FROM hospitals WHERE user_id = ?
+SELECT id, 'hospitals' AS type, title, description,price,city,state, images FROM hospitals WHERE user_id = ?
 UNION ALL
-SELECT id, 'property' AS type, title, description, images FROM property WHERE user_id = ?
+SELECT id, 'property' AS type, title, description,price,city,state, images FROM property WHERE user_id = ?
 UNION ALL
-SELECT id, 'vehicles' AS type, title, description, images FROM vehicles WHERE user_id = ?
+SELECT id, 'vehicles' AS type, title, description,price,city,state, images FROM vehicles WHERE user_id = ?
 `
