955fb3b1ed595bb65e2f3f7b182a880bf4f5e5ff
jay-ekodus <jsardar@ekodusinc.com>
2024-04-11 11:32:33 +0530

    updated vehicles-27

--- src/Features/Home/sqlQueries.js
+++ src/Features/Home/sqlQueries.js
@@ -74,26 +74,52 @@ LIMIT ? OFFSET ?;
 //     SELECT id, 'vehicles' AS type, title, description, images FROM vehicles WHERE MATCH(title, brand, type, description,street, city, locality, pincode) AGAINST ?
 // )`       
 
-export const searchAdd = `SELECT id, title, price, created_at, images, city, state, 'property' AS category FROM property WHERE MATCH (title, type, street, city, house_no, pincode) AGAINST ? 
-UNION ALL
-(
-    SELECT id, title, price, created_at, images, city, state, 'vehicles' AS category FROM vehicles WHERE MATCH (title, brand, type, description, street, city, locality, pincode) AGAINST ? 
+export const searchAdd = `(
+    SELECT id, title, price, created_at, images,city, state, 'property' AS category
+    FROM property
+    WHERE is_active = 1 AND MATCH(title, type, street, city, house_no, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-    SELECT id, title, price, created_at, images, city, state, 'hospitality' AS category FROM hospitality WHERE MATCH (title, name, type, description, street, city, locality, pincode) AGAINST ? 
+    SELECT id, title, price, created_at, images,city, state, 'vehicles' AS category
+    FROM vehicles
+    WHERE is_active = 1 AND MATCH(title, brand, type, description,street, city, locality, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-    SELECT id, title, price, created_at, images, city, state, 'education' AS category FROM education WHERE MATCH (title, domain, institution_name, description, street, city, locality, pincode) AGAINST ? 
+    SELECT id, title, price, created_at, images,city, state, 'hospitality' AS category
+    FROM hospitality
+    WHERE is_active = 1 AND MATCH(title, name, type, description,street, city, locality, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-    SELECT id, title, price_per_visit, created_at, images, city, state, 'doctors' AS category FROM doctors WHERE MATCH (title, expertise, description, street, city, locality, pincode) AGAINST ? 
+    SELECT id, title, price, created_at, images,city, state, 'education' AS category
+    FROM education
+    WHERE is_active = 1 AND MATCH(title, domain, institution_name, description,street, city, locality, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 UNION ALL
 (
-    SELECT id, title, price_registration, created_at, images, city, state, 'hospitals' AS category FROM hospitals WHERE MATCH (title, name, type, description, street, city, locality, pincode) AGAINST ? 
+    SELECT id, title, price_per_visit, created_at, images,city, state, 'doctors' AS category
+    FROM doctors
+    WHERE is_active = 1 AND MATCH(title, expertise, description,street, city, locality, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
+)
+UNION ALL
+(
+    SELECT id, title, price_registration, created_at, images,city, state, 'hospitals' AS category
+    FROM hospitals
+    WHERE is_active = 1 AND MATCH(title, name, type, description,street, city, locality, pincode) AGAINST ?
+    ORDER BY created_at DESC
+    LIMIT 10
 )
 ORDER BY FIELD(category, 'property', 'vehicles', 'hospitality', 'education', 'doctors', 'hospitals'), created_at DESC
-`
+LIMIT ? OFFSET ?;`
