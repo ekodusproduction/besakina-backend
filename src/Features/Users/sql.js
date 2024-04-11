9c19db3e791f822959d04ab7c211f2ff6b57c9c9
jay-ekodus <jsardar@ekodusinc.com>
2024-04-11 12:36:15 +0530

    updated vehicles-27

--- src/Features/Users/sql.js
+++ src/Features/Users/sql.js
@@ -1,32 +1,32 @@
 export const getAllPosts = `(
-    SELECT id, title, price, created_at, images,city, state, 'property' AS category
+    SELECT id, title, price, created_at, images,city, state,is_active, 'property' AS category
     FROM property
 )
 UNION ALL
 (
-    SELECT id, title, price, created_at, images,city, state, 'vehicles' AS category
+    SELECT id, title, price, created_at, images,city, state,is_active, 'vehicles' AS category
     FROM vehicles
 )
 UNION ALL
 (
-    SELECT id, title, price, created_at, images,city, state, 'hospitality' AS category
+    SELECT id, title, price, created_at, images,city, state,is_active, 'hospitality' AS category
     FROM hospitality
     
 )
 UNION ALL
 (
-    SELECT id, title, price, created_at, images,city, state, 'education' AS category
+    SELECT id, title, price, created_at, images,city, state,is_active, 'education' AS category
     FROM education
     
 )
 UNION ALL
 (
-    SELECT id, title, price_per_visit, created_at, images,city, state, 'doctors' AS category
+    SELECT id, title, price_per_visit, created_at, images,city, state,is_active, 'doctors' AS category
     FROM doctors WHERE user_id = ?
 )
 UNION ALL
 (
-    SELECT id, title, price_registration, created_at, images,city, state, 'hospitals' AS category
+    SELECT id, title, price_registration, created_at, images,city, state,is_active ,'hospitals' AS category
     FROM hospitals WHERE user_id = ?
 )
 ORDER BY FIELD(category, 'property', 'vehicles', 'hospitality', 'education', 'doctors', 'hospitals'), created_at DESC
