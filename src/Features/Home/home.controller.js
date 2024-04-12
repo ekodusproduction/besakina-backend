537ef97c49abe18132ba8c8aa3350759c22e4d7c
jay-ekodus <jsardar@ekodusinc.com>
2024-04-12 15:54:44 +0530

    added index--4

--- src/Features/Home/home.controller.js
+++ src/Features/Home/home.controller.js
@@ -36,7 +36,6 @@ export const searchAdds = async function (req, res, next) {
 
         const query = `
             ${searchAdd}
-            LIMIT ${limit};
         `;
         console.log("query ", query)
         const [rows, fields] = await connection.execute(query, [search, search, search, search, search, search]);
