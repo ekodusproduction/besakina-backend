dc35f8e163b7ac0f217c5d1f4fb9d454a4b1bc2d
jay-ekodus <jsardar@ekodusinc.com>
2024-04-19 14:36:38 +0530

    changed auth message-30

--- src/Features/Users/sql.js
+++ src/Features/Users/sql.js
@@ -31,8 +31,7 @@ ORDER BY FIELD(category, 'property', 'vehicles', 'hospitality', 'education', 'do
 LIMIT 100 OFFSET 0;`
 
 
-export const getUserAndPlan = `SELECT u.*, p.* AS plan
+export const getUserAndPlan = `SELECT u.*, p.* plan
 FROM users AS u
 LEFT JOIN plans AS p ON u.plan_id = p.id
-WHERE u.id = ?;
-`
+WHERE u.id = ?;`
