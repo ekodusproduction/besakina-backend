bebba4033d1cbdb101c5938330375a0440490039
jay-ekodus <jsardar@ekodusinc.com>
2024-04-19 15:14:49 +0530

    changed auth message-5

--- src/Features/Users/sql.js
+++ src/Features/Users/sql.js
@@ -39,22 +39,23 @@ LIMIT 100 OFFSET 0;`
 
 export const getUserAndPlan = `SELECT u.*, 
 (SELECT 
-     p.id AS plan_id, 
-     p.type AS plan_type, 
-     p.no_of_ads AS plan_no_of_ads, 
-     p.price AS plan_price, 
-     p.validity AS plan_validity, 
-     p.verification_badge AS plan_verification_badge, 
-     p.search_priority AS plan_search_priority, 
-     p.membership_badge AS plan_membership_badge, 
-     p.contact_limit AS plan_contact_limit, 
-     p.no_images AS plan_no_images, 
-     p.business_profile AS plan_business_profile, 
-     p.images_business_profile AS plan_images_business_profile, 
-     p.offer_price AS plan_offer_price
+     JSON_OBJECT(
+         'id', p.id,
+         'type', p.type,
+         'no_of_ads', p.no_of_ads,
+         'price', p.price,
+         'validity', p.validity,
+         'verification_badge', p.verification_badge,
+         'search_priority', p.search_priority,
+         'membership_badge', p.membership_badge,
+         'contact_limit', p.contact_limit,
+         'no_images', p.no_images,
+         'business_profile', p.business_profile,
+         'images_business_profile', p.images_business_profile,
+         'offer_price', p.offer_price
+     ) AS plan
  FROM plans AS p
  WHERE p.id = u.plan_id
 ) AS plan
 FROM users AS u
-WHERE u.id = ?;
-`
+WHERE u.id = ?;`
