68c8b36ad3e18ed4c9d0ab312457aac114259583
jay-ekodus <jsardar@ekodusinc.com>
2024-04-29 11:15:57 +0530

    added fix v1 4-27

--- src/Features/Advertisement/Hospitality/repository.js
+++ src/Features/Advertisement/Hospitality/repository.js
@@ -195,7 +195,7 @@ export const deleteImage = async (advertisementID, files) => {
         const [rows, fields] = await connection.query(sql, [advertisementID])
 
         if (rows[0].length == 0) {
-            return new ApplicationError("vehicles not found.", 404);
+            throw new ApplicationError("vehicles not found.", 404);
         }
         if (rows[0].images == []) {
             return { error: false, message: "Images deleted successfully from the vehicles" };
