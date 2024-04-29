0eb853e733c0c5f14600d95ac2ea3cb4be183e11
jay-ekodus <jsardar@ekodusinc.com>
2024-04-29 17:59:08 +0530

    fixed add images 4-29

--- src/ErrorHandler/applicationError.js
+++ src/ErrorHandler/applicationError.js
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 dotenv.config()
 export class ApplicationError extends Error {
     constructor(error, code) {
+        console.log(error)
         const message = process.env.NODE_ENVIRONMENT == "development" ? error : "Internal server error"
         super(message);
         this.code = code;
