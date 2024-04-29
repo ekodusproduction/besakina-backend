f4ea86a30ea735e15bc6a9c0d7402777ea13c74e
jay-ekodus <jsardar@ekodusinc.com>
2024-04-29 12:41:55 +0530

    added fix v1 4-29

--- src/Middlewares/validationMiddleware.js
+++ src/Middlewares/validationMiddleware.js
@@ -1,4 +1,4 @@
-import { sendError } from "../Utility/response"
+import { sendError } from "../Utility/response.js"
 
 export const requestBodyValidator = async function (req, res, next) {
     if (req.body == {}) {
