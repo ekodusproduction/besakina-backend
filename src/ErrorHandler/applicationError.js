7bc9ba963b33ca80e963ea719e4d812a63ae75be
jay-ekodus <jsardar@ekodusinc.com>
2024-04-30 16:04:27 +0530

    added delete api

--- src/ErrorHandler/applicationError.js
+++ src/ErrorHandler/applicationError.js
@@ -3,7 +3,7 @@ dotenv.config()
 export class ApplicationError extends Error {
     constructor(error, code) {
         const message = process.env.NODE_ENVIRONMENT == "development" ? error : "Internal server error"
-        console.log("message in err handler", message)
+        console.log("message in err handler", error)
         super(message);
         this.code = code;
     }
