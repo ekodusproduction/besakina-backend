3837db4d1ed32a2c0f752a7e6dfa01c10f7de7b4
jay-ekodus <jsardar@ekodusinc.com>
2024-05-06 12:00:18 +0530

    added fixes-1

--- src/Features/Advertisement/Vehicles/repository.js
+++ src/Features/Advertisement/Vehicles/repository.js
@@ -95,10 +95,10 @@ const filterAdvertisement = async (query) => {
         if (query?.minPrice) delete query.minPrice;
         if (query?.maxPrice) delete query.maxPrice;
 
-        const [sql, values] = await filterQuery("doctors", [], { is_active: 1, ...query }, rangeCondition);
+        const [sql, values] = await filterQuery("vehicles", [], { is_active: 1, ...query }, rangeCondition);
         const [rows, fields] = await connection.query(sql, values);
         const data = await parseImages(rows);
-        return { error: false, message: "doctors filter list", "data": data };
+        return { error: false, message: "vehicles filter list", "data": data };
     } catch (error) {
         logger.info(error);
         throw new ApplicationError(error, 500);
@@ -116,14 +116,14 @@ export const updateAdvertisement = async (advertisementID, updateBody, userId) =
         if (!updateBody || typeof updateBody !== 'object') {
             throw new ApplicationError("Invalid updateBody object provided", 400);
         }
-        const [sql, values] = await updateQuery("doctors", updateBody, { "id": advertisementID, "user_id": userId })
+        const [sql, values] = await updateQuery("vehicles", updateBody, { "id": advertisementID, "user_id": userId })
         const [rows, field] = await connection.query(sql, values)
 
         if (!rows) {
-            throw new ApplicationError("doctors not updated. No matching doctors found for the provided ID.", 404);
+            throw new ApplicationError("vehicles not updated. No matching vehicles found for the provided ID.", 404);
         }
 
-        return { error: false, message: "doctors updated successfully", "advertisements": rows };
+        return { error: false, message: "vehicles updated successfully", "advertisements": rows };
     } catch (error) {
         console.log("error in repo", error)
         logger.info(error);
@@ -136,14 +136,14 @@ export const updateAdvertisement = async (advertisementID, updateBody, userId) =
 export const deactivateAdvertisement = async (advertisementID, userId) => {
     let connection = await pool.getConnection();
     try {
-        const select = `SELECT * FROM doctors WHERE is_active = 1 AND id = ? AND user_id = ?`;
+        const select = `SELECT * FROM vehicles WHERE is_active = 1 AND id = ? AND user_id = ?`;
         const [advertisement, selectFields] = await connection.query(select, [advertisementID, userId]);
 
         if (advertisement.length == 0) {
             throw new ApplicationError("Advertisement not found", 500);
         }
 
-        const sql = `UPDATE doctors SET is_active = 0 WHERE id = ?`;
+        const sql = `UPDATE vehicles SET is_active = 0 WHERE id = ?`;
         const [rows, fields] = await connection.query(sql, [advertisementID]);
 
         return { error: false, message: "Advertisement deactivated successfully" };
@@ -159,20 +159,20 @@ export const addImage = async (advertisementID, files, userId) => {
     let connection = await pool.getConnection();
 
     try {
-        const [query, values] = await selectQuery("doctors", {}, { id: advertisementID, user_id: userId })
+        const [query, values] = await selectQuery("vehicles", {}, { id: advertisementID, user_id: userId })
         const [advertisement, field] = await connection.query(query, values);
         if (advertisement.length == 0) {
-            throw new ApplicationError("doctors not found.", 404);
+            throw new ApplicationError("vehicles not found.", 404);
         }
         const images = JSON.parse(advertisement[0].images || '[]');
 
         const filePaths = files.map(file => file.path);
         const photosJson = JSON.stringify([...filePaths, ...images]);
 
-        const [update, updateValues] = await updateQuery("doctors", { images: photosJson }, { id: advertisementID })
+        const [update, updateValues] = await updateQuery("vehicles", { images: photosJson }, { id: advertisementID })
 
         const [rows] = await connection.query(update, updateValues);
-        return { error: false, message: "Images added successfully to the doctors", data: filePaths };
+        return { error: false, message: "Images added successfully to the vehicles", data: filePaths };
     } catch (error) {
         logger.info(error);
         throw new ApplicationError(error, 500);
@@ -185,14 +185,14 @@ export const deleteImage = async (advertisementID, files, userId) => {
     let connection = await pool.getConnection();
 
     try {
-        const sql = `SELECT * FROM doctors WHERE id = ? AND user_id = ?`
+        const sql = `SELECT * FROM vehicles WHERE id = ? AND user_id = ?`
         const [rows, fields] = await connection.query(sql, [advertisementID, userId])
 
         if (rows[0].length == 0) {
-            throw new ApplicationError("doctors not found.", 404);
+            throw new ApplicationError("vehicles not found.", 404);
         }
         if (rows[0].images == []) {
-            return { error: false, message: "Images deleted successfully from the doctors" };
+            return { error: false, message: "Images deleted successfully from the vehicles" };
         }
 
         const parsedImages = JSON.parse(rows[0].images || []);
@@ -205,11 +205,11 @@ export const deleteImage = async (advertisementID, files, userId) => {
 
         const photosJson = JSON.stringify(images);
 
-        const updateSql = `UPDATE doctors SET images =? WHERE id = ?`
+        const updateSql = `UPDATE vehicles SET images =? WHERE id = ?`
 
         await connection.query(updateSql, [photosJson, advertisementID])
 
-        return { error: false, message: "Images deleted successfully from the doctors" };
+        return { error: false, message: "Images deleted successfully from the vehicles" };
     } catch (error) {
         logger.info(error);
         console.log("erro riu catch", error)
@@ -223,15 +223,15 @@ export const activateAdvertisement = async (advertisementID, userId) => {
     let connection = await pool.getConnection();
 
     try {
-        const [query, values] = await selectQuery('doctors', { is_active: 1 }, { id: advertisementID, user_id: userId })
+        const [query, values] = await selectQuery('vehicles', { is_active: 1 }, { id: advertisementID, user_id: userId })
         const [advertisement] = await connection.query(query, values);
 
         if (advertisement.length == 0) {
-            throw new ApplicationError("doctors not found.", 404);
+            throw new ApplicationError("vehicles not found.", 404);
         }
-        const [update, updateValues] = await updateQuery('doctors', { is_active: 1 }, { id: advertisementID })
+        const [update, updateValues] = await updateQuery('vehicles', { is_active: 1 }, { id: advertisementID })
         const [rows] = await connection.query(update, updateValues);
-        return { error: false, message: "doctors activated successfully", data: rows };
+        return { error: false, message: "vehicles activated successfully", data: rows };
     } catch (error) {
         logger.info(error);
         throw new ApplicationError(error, 500);
@@ -243,7 +243,7 @@ export const activateAdvertisement = async (advertisementID, userId) => {
 export const deleteAdvertisement = async (advertisementID, userId) => {
     let connection = await pool.getConnection();
     try {
-        const sql = `DELETE FROM doctors WHERE id = ? AND user_id = ?`
+        const sql = `DELETE FROM vehicles WHERE id = ? AND user_id = ?`
         const [advertisement] = await connection.query(sql, [advertisementID, userId]);
         return { error: false, message: "property deleted successfully" };
     } catch (error) {
