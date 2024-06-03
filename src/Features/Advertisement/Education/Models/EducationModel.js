819231f2800b085392b4e44157f9efce84e01e02
jay-ekodus <jsardar@ekodusinc.com>
2024-06-03 15:54:26 +0530

    added form apis-7

--- src/Features/Advertisement/Education/Models/EducationModel.js
+++ src/Features/Advertisement/Education/Models/EducationModel.js
@@ -3,7 +3,14 @@ import Base from '../../BaseModel/BaseModel.js';
 
 const educationSchema = new mongoose.Schema({
     type: { type: String, required: true },
-    domain: [{ type: String, required: true }],
+    domain: [{
+        type: String, required: true, validate: {
+            validator: function (arr) {
+                return arr.length > 0;
+            },
+            message: 'Domain cannot be empty'
+        }
+    }],
     institution_name: { type: String, required: true },
     course_duration: { type: String, required: true },
     price: { type: String, default: null },
