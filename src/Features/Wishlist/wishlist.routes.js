426f3b49d31a94bfd216c31e0119ee33130c7965
jay-ekodus <jsardar@ekodusinc.com>
2024-06-18 14:54:10 +0530

    added wishlist-2

--- src/Features/Wishlist/wishlist.routes.js
+++ src/Features/Wishlist/wishlist.routes.js
@@ -1,10 +1,9 @@
 import { Router } from "express";
 import { jwtAuth } from "../../Middlewares/auth.middleware.js";
-import { wishlistValidation } from "./wishlist.middleware.js";
 import { addWishListItem, getWishList, removeWishListItem } from "./wishlist.controller";
 const wishlistRoutes = Router()
 
-wishlistRoutes.post("/add", jwtAuth,  addWishListItem)
+wishlistRoutes.post("/add", jwtAuth, addWishListItem)
 wishlistRoutes.get("/get", jwtAuth, removeWishListItem)
 wishlistRoutes.delete("/delete/id/:id", jwtAuth, getWishList)
 
