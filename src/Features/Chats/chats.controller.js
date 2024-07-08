63699f1b7edd1bbe3b22c3321b735a7a9ce0ee7b
jay-ekodus <jsardar@ekodusinc.com>
2024-07-08 17:32:06 +0530

    added chats-23

--- src/Features/Chats/chats.controller.js
+++ src/Features/Chats/chats.controller.js
@@ -18,8 +18,8 @@ export const getChatRooms = async (req, res, next) => {
             {
                 "$match": {
                     "$or": [
-                        { "sender": ObjectId(userId) },
-                        { "receiver": ObjectId(userId) }
+                        { "sender": userId },
+                        { "receiver": userId }
                     ]
                 }
             },
@@ -73,14 +73,14 @@ export const getChatRooms = async (req, res, next) => {
                     "lastTimestamp": 1,
                     "sender": {
                         "$cond": {
-                            "if": { "$eq": ["$_id.receiver", ObjectId(userId)] },
+                            "if": { "$eq": ["$_id.receiver", userId] },
                             "then": { "$arrayElemAt": ["$senderDetails", 0] },
                             "else": { "fullname": "", "profile_pic": "" }
                         }
                     },
                     "receiver": {
                         "$cond": {
-                            "if": { "$eq": ["$_id.sender", ObjectId(userId)] },
+                            "if": { "$eq": ["$_id.sender", userId] },
                             "then": { "$arrayElemAt": ["$receiverDetails", 0] },
                             "else": { "fullname": "", "profile_pic": "" }
                         }
