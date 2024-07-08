26683b135a389e9b7f794b0c945561ca23d1ef9d
jay-ekodus <jsardar@ekodusinc.com>
2024-07-08 11:54:46 +0530

    added chats-3

--- src/Features/Chats/chat.socket.js
+++ src/Features/Chats/chat.socket.js
@@ -4,10 +4,10 @@ export const chatSocket = (socket) => {
 
     socket.on('join', async () => {
         try {
-            console.log("join event fired")
+            console.log("join event fired");
             const sender = socket.user;
-            console.log("sender", sender)
-            const roomId = sender
+            console.log("sender", sender);
+            const roomId = [receiver, sender].sort().join("_");
             socket.join(roomId);
             socket.emit('joinedRoom', { roomId, message: `You have joined room ${roomId}` });
             console.log(`User joined room: ${roomId}`);
@@ -18,22 +18,21 @@ export const chatSocket = (socket) => {
 
     socket.on("sendMessage", async (messageData) => {
         try {
-            console.log("sendMessage event fired")
+            console.log("sendMessage event fired");
 
             const { receiver } = messageData;
             const sender = socket.user;
-            console.log("sender", sender)
-            const roomId = receiver;
+            console.log("sender", sender);
+            const roomId = [receiver, sender].sort().join("_");
             messageData.roomId = roomId;
             messageData.sender = sender;
             const message = await Chat.create(messageData);
             messageData.user = sender;
 
-            console.log("message", message)
+            console.log("message", message);
             if (socket.rooms.has(roomId)) {
-                console.log("has room ")
+                console.log("has room ");
                 socket.to(roomId).emit("receivedMessage", message);
-                // socket.emit('receivedMessage', { roomId, message: message });
                 console.log(`Message sent to room: ${roomId}`, message);
             } else {
                 console.error(`Socket is not in the room: ${roomId}`);
@@ -45,16 +44,16 @@ export const chatSocket = (socket) => {
 
     socket.on("isActive", async (messageData) => {
         try {
-            console.log("isActive event fired")
+            console.log("isActive event fired");
 
             const sender = socket.user;
-            const roomId = sender
+            const roomId = sender; // Assuming sender is the unique identifier for the room in this context
             messageData.roomId = roomId;
 
             // Check if the socket is in the room before sending the isActive event
             if (socket.rooms.has(roomId)) {
                 console.log("has room ");
-                io.emit("isActive", { isActive: true, }); // Broadcast to everyone
+                io.emit("isActive", { isActive: true }); // Broadcast to everyone
                 console.log(`isActive event broadcast to all clients for room: ${roomId}`);
             } else {
                 console.error(`Socket is not in the room: ${roomId}`);
