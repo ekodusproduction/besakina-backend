f6452caca8eeca45cf562c72ec357405f2539e22
jay-ekodus <jsardar@ekodusinc.com>
2024-07-09 11:39:21 +0530

    added chats-33

--- redis.js
+++ redis.js
@@ -10,13 +10,31 @@ const redisPassword = process.env.REDIS_PASSWORD;
 const redisClient = redis.createClient({
     host: redisHost,
     port: redisPort,
-    password: redisPassword
+    password: redisPassword,
+    retry_strategy: function (options) {
+        if (options.error && options.error.code === 'ECONNREFUSED') {
+            // End reconnecting on a specific error and flush all commands with a individual error
+            console.error('The server refused the connection');
+            return new Error('The server refused the connection');
+        }
+        if (options.total_retry_time > 1000 * 60 * 60) {
+            // End reconnecting after a specific timeout and flush all commands with a individual error
+            console.error('Retry time exhausted');
+            return new Error('Retry time exhausted');
+        }
+        if (options.attempt > 10) {
+            // End reconnecting with built in error
+            console.error('Too many attempts to reconnect');
+            return undefined;
+        }
+        // Reconnect after
+        return Math.min(options.attempt * 100, 3000);
+    }
 });
 
 // Promisify Redis commands
 const asyncSet = promisify(redisClient.set).bind(redisClient);
 const asyncGet = promisify(redisClient.get).bind(redisClient);
-// Promisify Redis commands
 const asyncSadd = promisify(redisClient.sAdd).bind(redisClient);
 const asyncSrem = promisify(redisClient.sRem).bind(redisClient);
 const asyncSmembers = promisify(redisClient.sMembers).bind(redisClient);
@@ -28,6 +46,14 @@ redisClient.on('error', (err) => {
 
 redisClient.on('ready', () => {
     console.log('Redis client connected');
+});
+
+redisClient.on('end', () => {
+    console.log('Redis client disconnected');
+});
+
+redisClient.on('reconnecting', (delay, attempt) => {
+    console.log(`Reconnecting to Redis: attempt ${attempt}, delay ${delay}`);
 });
 
 export { asyncGet, asyncSadd, asyncSet, asyncSismember, asyncSmembers, asyncSrem };
