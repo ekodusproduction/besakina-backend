aa2a9f90f76941de1bba7f7c92d32c654d81f301
jay-ekodus <jsardar@ekodusinc.com>
2024-07-09 13:55:16 +0530

    added chats-44

--- src/Features/Chats/chats.controller.js
+++ src/Features/Chats/chats.controller.js
@@ -77,14 +77,14 @@ export const getChatRooms = async (req, res, next) => {
                         "$cond": {
                             "if": { "$eq": ["$_id.sender", new ObjectId(userId)] },
                             "then": { "$arrayElemAt": ["$recieverDetails", 0] },
-                            "else": {}
+                            "else": null
                         }
                     },
                     "reciever": {
                         "$cond": {
                             "if": { "$eq": ["$_id.reciever", new ObjectId(userId)] },
                             "then": { "$arrayElemAt": ["$senderDetails", 0] },
-                            "else": {}
+                            "else": null
                         }
                     }
                 }
