369271a91eec68ede8e4141ebdc213977a6b33b9
jay-ekodus <jsardar@ekodusinc.com>
2024-08-12 14:51:46 +0530

    added

--- src/Features/Advertisement/Hospitality/repository.js
+++ src/Features/Advertisement/Hospitality/repository.js
@@ -7,6 +7,7 @@ export const addAdvertisement = async (requestBody, files) => {
     try {
         requestBody.images = files;
         const result = new Hospitality(requestBody);
+        console.log(result);
         const savedDoctor = await result.save();
         if (!savedDoctor) {
             return { error: true, data: { message: "Error adding Hospitality.", statusCode: 400, data: null } };
