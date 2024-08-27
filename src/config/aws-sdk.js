cb5dc94823c6e2e8db8bb77d12cc2c8ef0648922
jay-ekodus <jsardar@ekodusinc.com>
2024-08-27 10:37:58 +0530

    added env

--- src/config/aws-sdk.js
+++ src/config/aws-sdk.js
@@ -4,15 +4,15 @@ import { S3, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { v4 as uuidv4 } from 'uuid';
 import { ApplicationError } from "../ErrorHandler/applicationError.js";
 
-const spaceBaseUrl = 'https://besakina.blr1.digitaloceanspaces.com';
+const spaceBaseUrl = process.env.SP_ORIGIN_ENDPOINT;
 // Define s3Client here
 export const s3Client = new S3Client({
-    region: "blr1",
+    region: process.env.SP_REGION,
     credentials: {
-        accessKeyId: process.env.DG_SPACES_KEY,
-        secretAccessKey: process.env.DG_SPACES_SECRET
+        accessKeyId: process.env.SP_SPACES_KEY,
+        secretAccessKey: process.env.SP_SPACES_SECRET
     },
-    endpoint: "https://blr1.digitaloceanspaces.com"
+    endpoint: spaceBaseUrl
 });
 
 const uploadToSpaces = async (file) => {
@@ -30,7 +30,7 @@ const uploadToSpaces = async (file) => {
         const fileUrl = `${spaceBaseUrl}/${params.Key}`;
         return { fieldname: file.fieldname, path: fileUrl };
     } catch (error) {
-        throw new ApplicationError('Failed to upload file to DigitalOcean Spaces', 400);
+        throw new ApplicationError('Failed to upload file to supabase', 400);
     }
 };
 
