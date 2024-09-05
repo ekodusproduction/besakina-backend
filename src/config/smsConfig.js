1f774d21ceb0b5433519e8c82ebf6686b9160c67
jay-ekodus <jay.sardar@zycelltech.com>
2024-09-05 14:54:25 +0530

    removed consoles

--- src/config/smsConfig.js
+++ src/config/smsConfig.js
@@ -4,26 +4,27 @@ import axios from "axios"
 
 const url = 'https://www.fast2sms.com/dev/bulkV2';
 const apiKey = process.env.API_KEY_FAST2SMS;
-export const sendSms = async function(messageId, variables, number) {
+
+export const sendSms = async function (messageId, variables, number) {
     const data = {
-      sender_id: process.env.DLT_SENDER_ID,
-      message: messageId,
-      variables_values: variables,
-      route: 'dlt',
-      numbers: number
+        sender_id: process.env.DLT_SENDER_ID,
+        message: messageId,
+        variables_values: variables,
+        route: 'dlt',
+        numbers: number
     };
-  
+
     try {
-      const response = await axios.post(url, new URLSearchParams(data), {
-        headers: {
-          'authorization': apiKey,
-          'Content-Type': 'application/x-www-form-urlencoded'
-        }
-      });
-      console.log(response.data);
-      return true; // Resolve to true if the request is successful
+        const response = await axios.post(url, new URLSearchParams(data), {
+            headers: {
+                'authorization': apiKey,
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        });
+        console.log(response.data);
+        return true;
     } catch (error) {
-      console.error('Error:', error.message);
-      return false; // Resolve to false if there is an error
+        console.error('Error fastsms:', error);
+        return false; 
     }
-  };
+};
