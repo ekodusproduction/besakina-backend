cb2cc713691d74e0e4042ffb549f875d73c80e27
jay-ekodus <jay.sardar@zycelltech.com>
2024-09-18 12:09:22 +0530

    imported files

--- src/Features/Banner/BannerModel.js
+++ src/Features/Banner/BannerModel.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const bannerSchema = new mongoose.Schema({
     type: { type: String, required: 'Please send type of banner' },
-    images: { type: String, required: 'Please send image of banner' },
+    images: [{ type: String, required: 'Please send image of banner' }],
     isActive:{type:Boolean, default:true},
     user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: 'Please send User Id' },
 
