2f571bfbf5ccde6eb35102486fb771d7fdb9afd3
jay-ekodus <jay.sardar@zycelltech.com>
2024-09-18 11:56:29 +0530

    imported files

--- src/Features/Banner/banner.routes.js
+++ src/Features/Banner/banner.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { jwtAuth } from "../../Middlewares/auth.middleware.js";
 import { getBanner, addBanner, editBanner, deleteBanner } from "./banner.controller.js"
-
+import { fileUpload } from "../../Middlewares/multer.middlewares.js";
 const bannerRouter = Router()
 
 bannerRouter.get("/", getBanner)
