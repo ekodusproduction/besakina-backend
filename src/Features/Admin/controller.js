c08cf26fd3118af43f0b1ea37f5ba78d7691e152
jay-ekodus <jay.sardar@zycelltech.com>
2024-10-23 14:51:30 +0530

    added rashmi number

--- src/Features/Admin/controller.js
+++ src/Features/Admin/controller.js
@@ -1,11 +1,16 @@
 import Base from "../Advertisement/BaseModel/BaseModel.js";
 import Business from "../BusinessListing/Model/BusinessModel.js";
-
+import User from "../Users/Models/UserModel.js";
 export const businessInfo = async (req, res, next) => {
     try {
-        let user = await Business.find({ is_active: true }).populate('user');
+        let user = await Business.find({ is_active: true }).select({
+            advType: 1, created_at: 1,
+            category: 1, street: 1, city: 1, location
+        }).populate('User').select({
+            plan: 1, fullname: 1, mobile: 1,
+        });
 
-        return await sendResponse(res, 'Business Info List', 201, user, null);
+        return await sendResponse(res, 'Business Info List', 200, user, null);
     } catch (error) {
         next(error);
     }
@@ -13,9 +18,14 @@ export const businessInfo = async (req, res, next) => {
 
 export const advertisementInfo = async (req, res, next) => {
     try {
-        let user = await Base.find({ is_active: true }).populate('user');
+        let user = await Base.find({ is_active: true }).select({
+            created_at: 1, category: 1, street: 1,
+            city: 1, location
+        }).populate('User').select({
+            plan: 1, fullname: 1, mobile: 1,
+        });
 
-        return await sendResponse(res, 'Advertisement Info List', 201, user, null);
+        return await sendResponse(res, 'Advertisement Info List', 200, user, null);
     } catch (error) {
         next(error);
     }
