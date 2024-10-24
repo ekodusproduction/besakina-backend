d704a0de0c60e8be3299fcdfe769213a222fcf50
jay-ekodus <jay.sardar@zycelltech.com>
2024-10-24 12:31:44 +0530

    added admin 23-10

--- src/Features/Admin/routes.js
+++ src/Features/Admin/routes.js
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { businessInfo, advertisementInfo } from "./controller.js";
 const adminRouter = Router()
 
-adminRouter.get("/advertisement/info", businessInfo)
-adminRouter.get("/business/info", advertisementInfo)
+adminRouter.get("/advertisement/info", advertisementInfo)
+adminRouter.get("/business/info", businessInfo)
 
 
 export default adminRouter  
